ad89f9c646f1ddc0fd3d13a75df594d02a376948
Paolo Chang <pchang@architech.ca>
2022-04-19 11:26:19 -0400

    React 7.4 ComponentTest HabitAddForm

---
+++ React/src/components/habitAddForm/test/habitAddForm.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import HabitAddForm from "../habitAddForm";
+
+describe("habitAddForm", () => {
+  let onAdd;
+  let input;
+  let button;
+  beforeEach(() => {
+    onAdd = jest.fn();
+    render(<HabitAddForm onAdd={onAdd} />);
+    input = screen.getByPlaceholderText("Habit");
+    button = screen.getByText("Add");
+  });
+
+  it("calls onAdd when button is clicked and valid habit is entered", () => {
+    userEvent.type(input, "New habit");
+    userEvent.click(button);
+    expect(onAdd).toHaveBeenCalledWith("New habit");
+  });
+
+  it("does not call onAdd when the habit is empty", () => {
+    userEvent.type(input, "");
+    userEvent.click(button);
+    expect(onAdd).toHaveBeenCalledTimes(0);
+  });
+});
